87aa8b863d96fe302d1aa82da6e33ea2616be08f
Eldar Gyulmagomedov <steal.vip@gmail.com>
2023-02-12 19:29:50 +0300

    Решили четвертый таск_?_ но VSC не выдает результат

---
+++ Task004/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе 
+// показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите число N:");
+int N = Convert.ToInt32(Console.ReadLine());
+int start = 0;
+while(start <= N);
+{
+    start = start + 2;
+    Console.WriteLine(start);  
+}
